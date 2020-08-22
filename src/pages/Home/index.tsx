36cdf706a0630b5f62a53f04bbcd47babc6a5114
Tiago Vaz <tiago.vaz@hotmail.com>
2020-08-22 12:39:09 -0300

    Handling loading, error and putting data on screen

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,28 +1,68 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import { gql } from '@apollo/client';
 import api from '../../services/api';
 
+interface Character {
+  id: number;
+  name: string;
+  status: string;
+  image: string;
+}
+
 const Home: React.FC = () => {
-  useEffect(() => {
-    window.document.title = 'Home';
-    api
-      .query({
-        query: gql`
-          query {
-            characters {
-              results {
-                name
-              }
+  const [loading, setLoading] = useState(true);
+  const [apiError, setApiError] = useState('');
+  const [characters, setCharacters] = useState<Character[]>([]);
+
+  const loadCharacters = useCallback(async () => {
+    const GET_CHARACTERS = {
+      query: gql`
+        query {
+          characters {
+            results {
+              id
+              name
+              status
+              image
             }
           }
-        `,
-      })
-      .then(response => console.log(response));
+        }
+      `,
+    };
+
+    try {
+      const response = await api.query(GET_CHARACTERS);
+      setCharacters(response.data.characters.results);
+    } catch (err) {
+      setApiError(err.message);
+    } finally {
+      setLoading(false);
+    }
   }, []);
+
+  useEffect(() => {
+    window.document.title = 'Home';
+    loadCharacters();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
+  if (apiError) {
+    return <p>{apiError}</p>;
+  }
+
   return (
     <div>
-      <h1>Home</h1>
-      <p>Listagem de personagens</p>
+      {characters.map(character => (
+        <div key={character.id}>
+          <h1>{character.name}</h1>
+          <h3>{character.status}</h3>
+          <img src={character.image} alt={character.name} />
+        </div>
+      ))}
     </div>
   );
 };
