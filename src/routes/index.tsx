3cc4b463aba701765a67d56cc1ba101b909160e8
Tiago Vaz <tiago.vaz@hotmail.com>
2020-08-22 13:44:35 -0300

    Not Found page created

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Details from '../pages/Details';
+import NotFound from '../pages/NotFound';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/details/:id" component={Details} />
     <Route path="/" exact component={Home} />
+    <Route component={NotFound} />
   </Switch>
 );
 
